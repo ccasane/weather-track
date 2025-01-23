f1fb1556a73a0ac68ecc73de170117d0c9362031
Joel Ccasane <joelccasane@gmail.com>
2025-01-23 18:30:31 -0500

    add main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
     HeaderComponent,
     SidebarComponent,
     FooterComponent,
+    MainComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   bootstrap: [AppComponent],
